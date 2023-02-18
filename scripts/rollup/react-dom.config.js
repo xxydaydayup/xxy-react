0aeeb18b40d6930215a964efa36c28e9fc9c0e97
xuxuxu <982792003@qq.com>
2023-02-18 15:43:34 +0800

    feat:"useState的实现"

--- scripts/rollup/react-dom.config.js
+++ scripts/rollup/react-dom.config.js
@@ -2,7 +2,7 @@ import { getPackageJSON, resolvePkgPath, getBaseRollupPlugins } from './utils';
 import generatePackageJson from 'rollup-plugin-generate-package-json';
 import alias from '@rollup/plugin-alias';
 
-const { name, module } = getPackageJSON('react-dom');
+const { name, module, peerDependencies } = getPackageJSON('react-dom');
 // react-dom包的路径
 const pkgPath = resolvePkgPath(name);
 // react-dom产物路径
@@ -24,6 +24,46 @@ export default [
 				format: 'umd'
 			}
 		],
+		external: [...Object.keys(peerDependencies)],
+		plugins: [
+			...getBaseRollupPlugins(),
+			// webpack resolve alias
+			alias({
+				entries: {
+					hostConfig: `${pkgPath}/src/hostConfig.ts`
+				}
+			}),
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, description, version }) => ({
+					name,
+					description,
+					version,
+					peerDependencies: {
+						react: version
+					},
+					main: 'index.js'
+				})
+			})
+		]
+	},
+	// react-dom
+	{
+		input: `${pkgPath}/${module}`,
+		output: [
+			{
+				file: `${pkgDistPath}/index.js`,
+				name: 'index.js',
+				format: 'umd'
+			},
+			{
+				file: `${pkgDistPath}/client.js`,
+				name: 'client.js',
+				format: 'umd'
+			}
+		],
+		external: [...Object.keys(peerDependencies)],
 		plugins: [
 			...getBaseRollupPlugins(),
 			// webpack resolve alias
